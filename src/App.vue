069adffe52b86f10a383dee96f8b042f2685006a
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-14 12:01:10 -0500

    menuitems

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,72 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>{{ appTitle }}</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
+  <main>
+    <v-app>
+      <v-navigation-drawer v-model="sidebar" app>
+        <v-list>
+          <v-list-tile
+            v-for="(item, index) in menuItems"
+            :key="index"
+            :to="{ name: item.link }"
+            exact
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="isAuthenticated" @click="userLogout">
+            <v-list-tile-action>
+              <v-icon>exit_to_app</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>Sign Out</v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app>
+        <span class="hidden-sm-and-up">
+          <v-toolbar-side-icon
+            @click="sidebar = !sidebar"
+          ></v-toolbar-side-icon>
+        </span>
+        <v-toolbar-title class="headline text-uppercase">
+          <router-link
+            :to="{ name: 'home' }"
+            tag="span"
+            style="cursor: pointer"
+            v-if="isAuthenticated"
+            >{{ appTitle }}</router-link
+          >
+          <router-link
+            :to="{ name: 'landing' }"
+            tag="span"
+            style="cursor: pointer"
+            v-else
+            >{{ appTitle }}</router-link
+          >
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn
+            flat
+            v-for="(item, index) in menuItems"
+            :key="index"
+            :to="{ name: item.link }"
+            exact
+          >
+            <v-icon>{{ item.icon }}</v-icon>
+            &nbsp;{{ item.title }}
+          </v-btn>
+          <v-btn flat v-if="isAuthenticated" @click="userLogout">
+            <v-icon left>exit_to_app</v-icon>Logout
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
 
-    <v-content>
-      <router-view />
-    </v-content>
-  </v-app>
+      <v-content>
+        <router-view />
+      </v-content>
+    </v-app>
+  </main>
 </template>
 
 <script>
@@ -20,11 +76,31 @@ export default {
   name: 'App',
   data() {
     return {
-      //
+      sidebar: false
     }
   },
   computed: {
-    ...mapGetters(['appTitle'])
+    ...mapGetters(['appTitle', 'isAuthenticated']),
+    menuItems() {
+      if (this.isAuthenticated) {
+        return [
+          { title: 'Home', link: 'home', icon: 'home' },
+          { title: 'About', link: 'about', icon: 'help_outline' }
+        ]
+      } else {
+        return [
+          { title: 'Home', link: 'landing', icon: 'home' },
+          { title: 'About', link: 'about', icon: 'help_outline' },
+          { title: 'Login', link: 'login', icon: 'lock' },
+          { title: 'Register', link: 'register', icon: 'face' }
+        ]
+      }
+    }
+  },
+  methods: {
+    userLogout() {
+      this.$store.dispatch('userLogout')
+    }
   }
 }
 </script>
