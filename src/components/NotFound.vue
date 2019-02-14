14651a2851c2f7eff8a8247dce4a1c6134f2592f
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-14 15:41:21 -0500

    not found routes

---
+++ src/components/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        <h1 class="display-2 font-weight-bold mb-3">404 PAGE NOT FOUND</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
