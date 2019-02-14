59b7129177111b42392463e2fdf99c8874abd6e7
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-14 12:00:01 -0500

    router home

---
+++ src/router/home.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    path: '/home',
+    name: 'home',
+    meta: {
+      requiresAuth: true
+    },
+    component: () =>
+      import(/* webpackChunkName: "home" */ '../components/Home.vue')
+  }
+]
