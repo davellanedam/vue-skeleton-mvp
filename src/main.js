bb08eda86427fb764449d59149c7a67ed4283ab1
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-14 12:01:22 -0500

    check auth

--- src/main.js
+++ src/main.js
@@ -10,5 +10,12 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    const isLoggedIn = this.$store.getters.isLoggedIn
+    console.log('IS LOGGED IN ?:', isLoggedIn)
+    if (isLoggedIn) {
+      store.dispatch('autoLogin', localStorage.getItem('user'))
+    }
+  }
 }).$mount('#app')
