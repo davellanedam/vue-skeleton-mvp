efb034c6ee0a37aa401afe73ecdb1c559d7c9346
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-14 11:59:26 -0500

    Protected home component

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">PROTECTED HOME</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
