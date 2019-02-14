3c763a191087b0c700ff2344ee09d1188fcb3b62
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-14 12:00:41 -0500

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import landing from './landing'
+import auth from './auth'
+import home from './home'
+import about from './about'
+import store from '@/store'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [...landing]
+  routes: [...landing, ...auth, ...home, ...about]
 })
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const isAuthenticated = store.getters.isAuthenticated
+  if (requiresAuth && !isAuthenticated) {
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+export default router
