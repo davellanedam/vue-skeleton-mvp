7ad4eae841bdb285e9ce7dd52de04039b6e1f9b1
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-14 15:39:31 -0500

    styling

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,7 @@
 <template>
-  <v-container>
-    <v-layout text-xs-center wrap>
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center" mt-5>
         <h1 class="display-2 font-weight-bold mb-3">ABOUT</h1>
       </v-flex>
     </v-layout>
