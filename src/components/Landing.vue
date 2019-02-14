99f1a515dd715b6073d27bc62341b80f6cd6c43d
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-14 11:58:49 -0500

    Landing component

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,7 +2,16 @@
   <v-container>
     <v-layout text-xs-center wrap>
       <v-flex xs12>
-        Landing page
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">LANDING</h1>
       </v-flex>
     </v-layout>
   </v-container>
