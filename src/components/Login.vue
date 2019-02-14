d5b052456c5a32a4dbe7f1228253b4433a8bff9d
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-14 11:21:38 -0500

    Login/Register components

---
+++ src/components/Login.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        <h1>Login</h1>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 mt-3>
+        <form @submit.prevent="userLogin">
+          <v-layout column>
+            <v-flex>
+              <v-alert type="error" dismissible v-model="alert">
+                {{ error }}
+              </v-alert>
+            </v-flex>
+            <v-flex>
+              <v-text-field
+                name="email"
+                label="Email"
+                id="email"
+                type="email"
+                v-model="email"
+                required
+              ></v-text-field>
+            </v-flex>
+            <v-flex>
+              <v-text-field
+                name="password"
+                label="Password"
+                id="password"
+                type="password"
+                v-model="password"
+                required
+              ></v-text-field>
+            </v-flex>
+            <v-flex class="text-xs-center" mt-5>
+              <v-btn color="primary" type="submit" :disabled="loading"
+                >Login</v-btn
+              >
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      alert: false
+    }
+  },
+  methods: {
+    userLogin() {
+      this.$store.dispatch('userLogin', {
+        email: this.email,
+        password: this.password
+      })
+    }
+  },
+  computed: {
+    error() {
+      return this.$store.state.error
+    },
+    loading() {
+      return this.$store.state.loading
+    }
+  },
+  watch: {
+    error(value) {
+      if (value) {
+        this.alert = true
+      }
+    },
+    alert(value) {
+      if (!value) {
+        this.$store.commit('setError', null)
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/Register.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        <h1>Register</h1>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 mt-3>
+        <form @submit.prevent="userRegister">
+          <v-layout column>
+            <v-flex>
+              <v-alert type="error" dismissible v-model="alert">
+                {{ error }}
+              </v-alert>
+            </v-flex>
+            <v-flex>
+              <v-text-field
+                name="email"
+                label="Email"
+                id="email"
+                type="email"
+                v-model="email"
+                required
+              ></v-text-field>
+            </v-flex>
+            <v-flex>
+              <v-text-field
+                name="password"
+                label="Password"
+                id="password"
+                type="password"
+                v-model="password"
+                required
+              ></v-text-field>
+            </v-flex>
+            <v-flex>
+              <v-text-field
+                name="confirmPassword"
+                label="Confirm Password"
+                id="confirmPassword"
+                type="password"
+                v-model="passwordConfirm"
+                :rules="[comparePasswords]"
+                required
+              ></v-text-field>
+            </v-flex>
+            <v-flex class="text-xs-center" mt-5>
+              <v-btn color="primary" type="submit" :disabled="loading"
+                >Register</v-btn
+              >
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      alert: false
+    }
+  },
+  computed: {
+    comparePasswords() {
+      return this.password === this.passwordConfirm
+        ? true
+        : "Passwords don't match"
+    },
+    error() {
+      return this.$store.state.error
+    },
+    loading() {
+      return this.$store.state.loading
+    }
+  },
+  methods: {
+    userRegister() {
+      if (this.comparePasswords !== true) {
+        return
+      }
+      this.$store.dispatch('userRegister', {
+        email: this.email,
+        password: this.password
+      })
+    }
+  },
+  watch: {
+    error(value) {
+      if (value) {
+        this.alert = true
+      }
+    },
+    alert(value) {
+      if (!value) {
+        this.$store.commit('setError', null)
+      }
+    }
+  }
+}
+</script>
