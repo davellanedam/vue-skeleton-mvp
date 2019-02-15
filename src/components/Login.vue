4b7878b30c846095f485e3e374153015b4f790b4
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-15 16:43:15 -0500

    styling

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <v-layout row wrap>
       <v-flex xs12 class="text-xs-center" mt-5>
-        <h1>Login</h1>
+        <h1 class="display-2 font-weight-bold mb-3">LOGIN</h1>
       </v-flex>
       <v-flex xs12 sm6 offset-sm3 mt-3>
         <form @submit.prevent="userLogin">
