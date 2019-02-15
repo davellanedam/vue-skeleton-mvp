7b305c60bba3a1ea5906d2af72d22cbd21f98671
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-15 16:43:21 -0500

    comments

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,10 +1,10 @@
 import camelCase from 'lodash/camelCase'
-const requireModule = require.context('.', false, /\.js$/) //extract js files inside modules folder
+const requireModule = require.context('.', false, /\.js$/) // Get js files inside modules folder
 const modules = {}
 
 requireModule.keys().forEach(fileName => {
-  if (fileName === './index.js') return //reject the index.js file
-  const moduleName = camelCase(fileName.replace(/(\.\/|\.js)/g, '')) //
+  if (fileName === './index.js') return // Avoid the index.js file
+  const moduleName = camelCase(fileName.replace(/(\.\/|\.js)/g, ''))
   modules[moduleName] = requireModule(fileName).default
 })
 
