2022af62610ed0c4efdc981b7e16915e88e7a3cb
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-24 22:11:24 -0500

    store i18n

--- src/store/modules/profile.js
+++ src/store/modules/profile.js
@@ -1,5 +1,6 @@
-import router from '@/router'
+import * as types from '../mutation-types'
 import axios from 'axios'
+import i18n from '@/i18n.js'
 
 const state = {}
 
@@ -12,10 +13,20 @@ const actions = {
       .then(response => {
         console.log('SUCCESS! ->', response)
         if (response.status === 200) {
+          commit(types.SHOW_LOADING, false)
+          commit(types.ERROR, null)
         } else {
+          commit(types.SHOW_LOADING, false)
         }
       })
-      .catch(error => {})
+      .catch(error => {
+        // Catches error connection or any other error (checks if error.response exists)
+        let errMsg = error.response
+          ? i18n.t(`myProfile.${error.response.data.errors.message}`)
+          : i18n.t('common.SERVER_TIMEOUT_CONNECTION_ERROR')
+        commit(types.SHOW_LOADING, false)
+        commit(types.ERROR, errMsg)
+      })
   }
 }
 
