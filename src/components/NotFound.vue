ab8c4dd5fa02c6e9301c77b380caac319c342a17
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-24 22:10:01 -0500

    translation

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -2,7 +2,9 @@
   <v-container fluid>
     <v-layout row wrap>
       <v-flex xs12 class="text-xs-center" mt-5>
-        <h1 class="display-2 font-weight-bold mb-3">404 PAGE NOT FOUND</h1>
+        <h1 class="display-2 font-weight-bold mb-3">
+          {{ $t('common.404') }}
+        </h1>
       </v-flex>
     </v-layout>
   </v-container>
