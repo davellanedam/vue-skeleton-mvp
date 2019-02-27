6e7da01c3b259bbf3e7ede8bd685fe6c4a75adef
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-26 19:15:15 -0500

    new translation location

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -3,7 +3,7 @@
     <v-layout row wrap>
       <v-flex xs12 class="text-xs-center" mt-5>
         <h1 class="display-2 font-weight-bold mb-3">
-          {{ $t('common.404') }}
+          {{ $t('errors.404') }}
         </h1>
       </v-flex>
     </v-layout>
