6a4b01796654d473eb994d64803b0f4378ad6e08
Daniel Avellaneda <davellanedam@gmail.com>
2019-02-27 15:21:32 -0500

    axios Accept-Language header

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import axios from 'axios'
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL || ''
+axios.defaults.headers.common['Accept-Language'] =
+  JSON.parse(localStorage.getItem('locale')) || 'en'
 
 axios.interceptors.request.use(
   function(config) {
