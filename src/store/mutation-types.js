19a4bebf7dfb715e2da2d1d63b85c31ded5853b2
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-02 16:22:18 -0500

    success message and email verification mutation types

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -2,6 +2,8 @@
 export const SET_LOCALE = 'SET_LOCALE'
 // error
 export const ERROR = 'ERROR'
+// success
+export const SUCCESS = 'SUCCESS'
 // loading
 export const SHOW_LOADING = 'SHOW_LOADING'
 // auth
@@ -9,3 +11,4 @@ export const IS_LOGGED_IN = 'IS_LOGGED_IN'
 export const SAVE_TOKEN = 'SAVE_TOKEN'
 export const SAVE_USER = 'SAVE_USER'
 export const LOGOUT = 'LOGOUT'
+export const EMAIL_VERIFIED = 'EMAIL_VERIFIED'
