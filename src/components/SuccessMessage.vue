f8513fc02db95bce1bcbb024fc728657705fbef4
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-02 16:21:54 -0500

    success message component

---
+++ src/components/SuccessMessage.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <v-alert
+          v-model="showSuccessMessage"
+          type="success"
+          dismissible
+          transition="scale-transition"
+        >
+          {{ successMessage }}
+        </v-alert>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import * as types from '../store/mutation-types'
+
+export default {
+  data() {
+    return {
+      showSuccessMessage: false
+    }
+  },
+  computed: {
+    successMessage() {
+      return this.$i18n.t(this.$store.state.success.successMessage)
+    }
+  },
+  watch: {
+    successMessage(value) {
+      if (value) {
+        this.showSuccessMessage = true
+      }
+    },
+    showSuccessMessage(value) {
+      if (!value) {
+        this.$store.commit(types.SUCCESS, null)
+      }
+    }
+  }
+}
+</script>
