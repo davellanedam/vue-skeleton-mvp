1441b489ef936b25c1e48a36866fe2dd6ba7d25c
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-02 14:20:20 -0500

    loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog v-model="showLoading" max-width="290" persistent>
+    <v-card color="blue white--text">
+      <v-card-title class="headline justify-center">{{
+        $t('loading.PROCESSING')
+      }}</v-card-title>
+      <v-spacer></v-spacer>
+      <div class="text-xs-center">
+        <v-progress-circular
+          indeterminate
+          v-bind:size="70"
+          v-bind:width="7"
+          color="white"
+        >
+        </v-progress-circular>
+      </div>
+      <v-card-text class="text-xs-center">
+        {{ $t('loading.PLEASE_WAIT') }}
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: mapGetters(['showLoading'])
+}
+</script>
