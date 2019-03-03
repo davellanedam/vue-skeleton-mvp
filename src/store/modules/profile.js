e2c9d0ab8dd73220376034706be91fb64c856c01
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-02 19:15:51 -0500

    store profile

--- src/store/modules/profile.js
+++ src/store/modules/profile.js
@@ -1,17 +1,56 @@
 import * as types from '../mutation-types'
 import axios from 'axios'
 
-const state = {}
+const state = {
+  profile: {
+    role: '',
+    verified: false,
+    name: '',
+    email: '',
+    phone: '',
+    city: '',
+    country: '',
+    urlTwitter: '',
+    urlGitHub: ''
+  },
+  cities: []
+}
 
-const getters = {}
+const getters = {
+  profile: state => state.profile,
+  cities: state => state.cities
+}
 
 const actions = {
+  getCities({ commit }) {
+    axios
+      .get('/cities/all')
+      .then(response => {
+        if (response.status === 200) {
+          const cities = []
+          const array = response.data
+          array.forEach(element => {
+            cities.push(element.name)
+          })
+          commit(types.FILL_CITIES, cities)
+        }
+      })
+      .catch(error => {
+        // Catches error connection or any other error (checks if error.response exists)
+        let errMsg = error.response
+          ? error.response.data.errors.msg
+          : 'SERVER_TIMEOUT_CONNECTION_ERROR'
+        commit(types.SHOW_LOADING, false)
+        commit(types.ERROR, errMsg)
+      })
+  },
   getProfile({ commit }) {
+    commit(types.SHOW_LOADING, true)
     axios
       .get('/profile')
       .then(response => {
-        console.log('SUCCESS! ->', response)
         if (response.status === 200) {
+          commit(types.FILL_PROFILE, response.data)
           commit(types.SHOW_LOADING, false)
           commit(types.ERROR, null)
         } else {
@@ -26,10 +65,83 @@ const actions = {
         commit(types.SHOW_LOADING, false)
         commit(types.ERROR, errMsg)
       })
+  },
+  saveProfile({ commit }, payload) {
+    commit(types.SHOW_LOADING, true)
+    const data = {
+      name: payload.name,
+      phone: payload.phone,
+      city: payload.city,
+      country: payload.country,
+      urlTwitter: payload.urlTwitter,
+      urlGitHub: payload.urlGitHub
+    }
+    axios
+      .patch('/profile', data)
+      .then(response => {
+        if (response.status === 200) {
+          commit(types.FILL_PROFILE, response.data)
+          commit(types.SUCCESS, 'myProfile.PROFILE_SAVED_SUCCESSFULLY')
+          commit(types.SHOW_LOADING, false)
+          commit(types.ERROR, null)
+        } else {
+          commit(types.SHOW_LOADING, false)
+        }
+      })
+      .catch(error => {
+        // Catches error connection or any other error (checks if error.response exists)
+        let errMsg = error.response
+          ? error.response.data.errors.msg
+          : 'SERVER_TIMEOUT_CONNECTION_ERROR'
+        commit(types.SHOW_LOADING, false)
+        commit(types.ERROR, errMsg)
+      })
+  },
+  addProfileData({ commit }, data) {
+    commit(types.ADD_PROFILE_DATA, data)
   }
 }
 
-const mutations = {}
+const mutations = {
+  [types.FILL_CITIES](state, cities) {
+    state.cities = cities
+  },
+  [types.FILL_PROFILE](state, data) {
+    state.profile.role = data.role
+    state.profile.verified = data.verified
+    state.profile.name = data.name
+    state.profile.email = data.email
+    state.profile.phone = data.phone
+    state.profile.city = data.city
+    state.profile.country = data.country
+    state.profile.urlTwitter = data.urlTwitter
+    state.profile.urlGitHub = data.urlGitHub
+  },
+  [types.ADD_PROFILE_DATA](state, data) {
+    switch (data.key) {
+      case 'name':
+        state.profile.name = data.value
+        break
+      case 'phone':
+        state.profile.phone = data.value
+        break
+      case 'city':
+        state.profile.city = data.value
+        break
+      case 'country':
+        state.profile.country = data.value
+        break
+      case 'urlTwitter':
+        state.profile.urlTwitter = data.value
+        break
+      case 'urlGitHub':
+        state.profile.urlGitHub = data.value
+        break
+      default:
+        break
+    }
+  }
+}
 
 export default {
   state,
