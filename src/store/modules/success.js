62ca7e84fe15af8cb5f7968f4aeae3119519b887
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-06 18:34:23 -0500

    success gets an object to be able to translate params

--- src/store/modules/success.js
+++ src/store/modules/success.js
@@ -1,16 +1,21 @@
 import * as types from '../mutation-types'
 
 const state = {
-  successMessage: null
+  successMessage: null,
+  successMessageParams: []
 }
 
 const getters = {
-  successMessage: state => state.successMessage
+  successMessage: state => state.successMessage,
+  successMessageParams: state => state.successMessageParams
 }
 
 const mutations = {
-  [types.SUCCESS](state, value) {
-    state.successMessage = value
+  [types.SUCCESS](state, payload) {
+    state.successMessage = payload.msg
+    if (payload.params) {
+      state.successMessageParams = payload.params
+    }
   }
 }
 
