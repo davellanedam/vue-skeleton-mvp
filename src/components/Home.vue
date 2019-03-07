919436d3a49a41390cf5cfb99c0060b67efdfbc1
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-06 20:47:27 -0500

    email verification dialog

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,29 @@
 <template>
   <v-container fluid>
     <v-layout row wrap>
+      <div class="text-xs-center">
+        <v-dialog v-model="emailVerified" width="500" persistent>
+          <v-card>
+            <v-card-title
+              class="headline grey lighten-2 black--text"
+              primary-title
+            >
+              <v-icon class="orange--text">warning</v-icon>
+              &nbsp;{{ $t('home.VERIFY_YOUR_ACCOUNT') }}
+            </v-card-title>
+            <v-card-text>
+              {{ $t('home.VERIFY_YOUR_ACCOUNT_DESCRIPTION') }}
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" flat @click="emailVerified = false">
+                {{ $t('home.CLOSE') }}
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
       <v-flex xs12 text-xs-center mt-5 mb-3>
         <h1 class="display-2 font-weight-bold">{{ $t('home.TITLE') }}</h1>
       </v-flex>
@@ -10,3 +33,13 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      emailVerified: !this.$store.state.emailVerified
+    }
+  }
+}
+</script>
