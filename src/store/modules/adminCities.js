78c5ae3a0618f4f7b26f643d05f7d0ef496598ca
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-08 13:12:39 -0500

    admin cities store

---
+++ src/store/modules/adminCities.js
@@ -0,0 +1,53 @@
+import * as types from '../mutation-types'
+import axios from 'axios'
+
+const state = {
+  cities: [],
+  totalCities: 0
+}
+
+const getters = {
+  cities: state => state.cities,
+  totalCities: state => state.totalCities
+}
+
+const actions = {
+  getCities({ commit }, payload) {
+    console.log('entro')
+    console.log(payload)
+    axios
+      .get('/cities', {
+        params: payload
+      })
+      .then(response => {
+        if (response.status === 200) {
+          commit(types.CITIES, response.data.docs)
+          commit(types.TOTAL_CITIES, response.data.totalDocs)
+        }
+      })
+      .catch(error => {
+        // Catches error connection or any other error (checks if error.response exists)
+        let errMsg = error.response
+          ? error.response.data.errors.msg
+          : 'SERVER_TIMEOUT_CONNECTION_ERROR'
+        commit(types.SHOW_LOADING, false)
+        commit(types.ERROR, errMsg)
+      })
+  }
+}
+
+const mutations = {
+  [types.CITIES](state, cities) {
+    state.cities = cities
+  },
+  [types.TOTAL_CITIES](state, value) {
+    state.totalCities = value
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
