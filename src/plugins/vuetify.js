f2d7929e4bc10c8fdd24892399536c90f6dc7d48
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-08 22:06:45 -0500

    use dialog confirm

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 import colors from 'vuetify/es5/util/colors'
+import VuetifyConfirm from 'vuetify-confirm'
 
 Vue.use(Vuetify, {
   theme: {
@@ -11,3 +12,4 @@ Vue.use(Vuetify, {
   },
   iconfont: 'md'
 })
+Vue.use(VuetifyConfirm)
