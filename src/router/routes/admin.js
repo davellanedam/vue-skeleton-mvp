c96be76fae83fcfebdda643bc40eae591726646f
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-09 21:46:30 -0500

    admin users route

--- src/router/routes/admin.js
+++ src/router/routes/admin.js
@@ -4,5 +4,11 @@ export default [
     name: 'admin-cities',
     component: () =>
       import(/* webpackChunkName: "admin-cities" */ '@/components/AdminCities.vue')
+  },
+  {
+    path: '/admin/users',
+    name: 'admin-users',
+    component: () =>
+      import(/* webpackChunkName: "admin-users" */ '@/components/AdminUsers.vue')
   }
 ]
