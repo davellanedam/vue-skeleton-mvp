1e8dbfca5b8c2ee82dab19cc9f06d76a945698da
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-11 14:45:47 -0500

    forgot password tests

---
+++ tests/e2e/specs/4.forgot.js
@@ -0,0 +1,35 @@
+import { setLocaleToEN } from '../support/utils'
+
+describe('Forgot Password', () => {
+  it('Visits the forgot password url', () => {
+    cy.visit('/forgot')
+    setLocaleToEN()
+    cy.get('h1')
+      .should('have.class', 'display-2')
+      .and('contain', 'Forgot my password')
+  })
+  it('Displays errors when user does not exist', () => {
+    cy.get('input[name=email]')
+      .clear()
+      .type('asdasd@asdasd.com{enter}')
+
+    cy.get('div.error')
+      .should('be.visible')
+      .and('contain', 'User does not exists')
+
+    // and still be on the same URL
+    cy.url().should('include', '/forgot')
+  })
+  it('Forgot password', () => {
+    cy.get('input[name=email]')
+      .clear()
+      .type('admin@admin.com{enter}')
+
+    cy.get('div.success')
+      .should('be.visible')
+      .and(
+        'contain',
+        'We have sent a message to admin@admin.com with instructions to recover your password'
+      )
+  })
+})
