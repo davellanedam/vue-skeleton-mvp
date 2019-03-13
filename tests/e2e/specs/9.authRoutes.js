3d4bd6b93a5766da1707af410a3d1c1c7cb228ca
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-13 00:38:00 -0500

    tests auth routes

---
+++ tests/e2e/specs/9.authRoutes.js
@@ -0,0 +1,42 @@
+describe('Auth routes', () => {
+  it('Forbids visits to home url if not authenticated', () => {
+    cy.visit('/')
+    cy.setLocaleToEN()
+    cy.visit('/home')
+    cy.get('h1')
+      .should('have.class', 'display-2')
+      .contains('Login')
+    // url should be /login
+    cy.url().should('include', '/login')
+  })
+  it('Forbids visits to profile url if not authenticated', () => {
+    cy.visit('/')
+    cy.setLocaleToEN()
+    cy.visit('/profile')
+    cy.get('h1')
+      .should('have.class', 'display-2')
+      .contains('Login')
+    // url should be /login
+    cy.url().should('include', '/login')
+  })
+  it('Forbids visits to admin cities url if not authenticated', () => {
+    cy.visit('/')
+    cy.setLocaleToEN()
+    cy.visit('/admin/cities')
+    cy.get('h1')
+      .should('have.class', 'display-2')
+      .contains('Login')
+    // url should be /login
+    cy.url().should('include', '/login')
+  })
+  it('Forbids visits to admin users url if not authenticated', () => {
+    cy.visit('/')
+    cy.setLocaleToEN()
+    cy.visit('/admin/users')
+    cy.get('h1')
+      .should('have.class', 'display-2')
+      .contains('Login')
+    // url should be /login
+    cy.url().should('include', '/login')
+  })
+})
