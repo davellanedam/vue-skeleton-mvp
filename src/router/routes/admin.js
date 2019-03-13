db9a45fb5b1345c981280fdfa6cae045b40f1c27
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-13 00:37:49 -0500

    admin routes require auth

--- src/router/routes/admin.js
+++ src/router/routes/admin.js
@@ -2,12 +2,18 @@ export default [
   {
     path: '/admin/cities',
     name: 'admin-cities',
+    meta: {
+      requiresAuth: true
+    },
     component: () =>
       import(/* webpackChunkName: "admin-cities" */ '@/components/AdminCities.vue')
   },
   {
     path: '/admin/users',
     name: 'admin-users',
+    meta: {
+      requiresAuth: true
+    },
     component: () =>
       import(/* webpackChunkName: "admin-users" */ '@/components/AdminUsers.vue')
   }
