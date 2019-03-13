4850d4367fd87c7765875f5813938f6e885aecc8
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-12 19:42:32 -0500

    profile test

---
+++ tests/e2e/specs/7.profile.js
@@ -0,0 +1,12 @@
+// import faker from 'faker'
+
+describe('Profile', () => {
+  it('Visits the profile url', () => {
+    cy.login()
+    cy.setLocaleToEN()
+    cy.visit('/profile')
+    cy.get('h1')
+      .should('have.class', 'display-2')
+      .and('contain', 'My profile')
+  })
+})
