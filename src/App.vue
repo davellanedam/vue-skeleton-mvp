9c5670ea4df30c6803366f0caf8e5c3dbb42339c
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-16 15:03:54 -0500

    <main> tag removed

--- src/App.vue
+++ src/App.vue
@@ -1,120 +1,114 @@
 <template>
-  <main>
-    <v-app dark>
-      <v-navigation-drawer v-model="sidebar" app disable-resize-watcher>
-        <v-list>
+  <v-app dark>
+    <v-navigation-drawer v-model="sidebar" app disable-resize-watcher>
+      <v-list>
+        <v-list-tile
+          v-for="(item, index) in menuItems"
+          :key="index"
+          :to="{ name: item.link }"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-group v-if="admin" prepend-icon="build" no-action>
+          <v-list-tile slot="activator">
+            <v-list-tile-title>{{ $t('adminItems.ADMIN') }}</v-list-tile-title>
+          </v-list-tile>
           <v-list-tile
-            v-for="(item, index) in menuItems"
+            v-for="(item, index) in adminItems"
             :key="index"
             :to="{ name: item.link }"
             exact
           >
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
             <v-list-tile-content>{{ item.title }}</v-list-tile-content>
           </v-list-tile>
+        </v-list-group>
 
-          <v-list-group v-if="admin" prepend-icon="build" no-action>
-            <v-list-tile slot="activator">
-              <v-list-tile-title>{{
-                $t('adminItems.ADMIN')
-              }}</v-list-tile-title>
-            </v-list-tile>
+        <v-list-tile v-if="isTokenSet" @click="userLogout">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{
+            $t('menuItems.LOGOUT')
+          }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <span class="hidden-md-and-up">
+        <v-toolbar-side-icon @click="sidebar = !sidebar"></v-toolbar-side-icon>
+      </span>
+      <v-toolbar-title class="headline text-uppercase">
+        <router-link
+          :to="{ name: 'home' }"
+          tag="span"
+          style="cursor: pointer"
+          v-if="isTokenSet"
+          >{{ appTitle }}</router-link
+        >
+        <router-link
+          :to="{ name: 'landing' }"
+          tag="span"
+          style="cursor: pointer"
+          v-else
+          >{{ appTitle }}</router-link
+        >
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn
+          flat
+          v-for="(item, index) in menuItems"
+          :key="index"
+          :to="{ name: item.link }"
+          exact
+          :class="['hidden-sm-and-down', item.class]"
+        >
+          <v-icon>{{ item.icon }}</v-icon>
+          &nbsp;{{ item.title }}
+        </v-btn>
+
+        <v-menu v-if="admin" offset-y class="hidden-sm-and-down">
+          <v-btn slot="activator" flat class="btnAdmin">
+            <v-icon>build</v-icon>
+            &nbsp;{{ $t('adminItems.ADMIN') }}
+          </v-btn>
+          <v-list>
             <v-list-tile
+              active-class="white--text"
               v-for="(item, index) in adminItems"
               :key="index"
               :to="{ name: item.link }"
               exact
+              :class="[item.class]"
             >
-              <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
             </v-list-tile>
-          </v-list-group>
+          </v-list>
+        </v-menu>
 
-          <v-list-tile v-if="isTokenSet" @click="userLogout">
-            <v-list-tile-action>
-              <v-icon>exit_to_app</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>{{
-              $t('menuItems.LOGOUT')
-            }}</v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <v-toolbar app>
-        <span class="hidden-md-and-up">
-          <v-toolbar-side-icon
-            @click="sidebar = !sidebar"
-          ></v-toolbar-side-icon>
-        </span>
-        <v-toolbar-title class="headline text-uppercase">
-          <router-link
-            :to="{ name: 'home' }"
-            tag="span"
-            style="cursor: pointer"
-            v-if="isTokenSet"
-            >{{ appTitle }}</router-link
-          >
-          <router-link
-            :to="{ name: 'landing' }"
-            tag="span"
-            style="cursor: pointer"
-            v-else
-            >{{ appTitle }}</router-link
-          >
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn
-            flat
-            v-for="(item, index) in menuItems"
-            :key="index"
-            :to="{ name: item.link }"
-            exact
-            :class="['hidden-sm-and-down', item.class]"
-          >
-            <v-icon>{{ item.icon }}</v-icon>
-            &nbsp;{{ item.title }}
-          </v-btn>
+        <v-btn
+          flat
+          v-if="isTokenSet"
+          @click="userLogout"
+          class="hidden-sm-and-down btnLogout"
+        >
+          <v-icon left>exit_to_app</v-icon>
+          {{ $t('menuItems.LOGOUT') }}
+        </v-btn>
+        <LocaleChanger />
+      </v-toolbar-items>
+    </v-toolbar>
 
-          <v-menu v-if="admin" offset-y class="hidden-sm-and-down">
-            <v-btn slot="activator" flat class="btnAdmin">
-              <v-icon>build</v-icon>
-              &nbsp;{{ $t('adminItems.ADMIN') }}
-            </v-btn>
-            <v-list>
-              <v-list-tile
-                active-class="white--text"
-                v-for="(item, index) in adminItems"
-                :key="index"
-                :to="{ name: item.link }"
-                exact
-                :class="[item.class]"
-              >
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile>
-            </v-list>
-          </v-menu>
-
-          <v-btn
-            flat
-            v-if="isTokenSet"
-            @click="userLogout"
-            class="hidden-sm-and-down btnLogout"
-          >
-            <v-icon left>exit_to_app</v-icon>
-            {{ $t('menuItems.LOGOUT') }}
-          </v-btn>
-          <LocaleChanger />
-        </v-toolbar-items>
-      </v-toolbar>
-
-      <v-content>
-        <loading />
-        <router-view />
-      </v-content>
-    </v-app>
-  </main>
+    <v-content>
+      <loading />
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
