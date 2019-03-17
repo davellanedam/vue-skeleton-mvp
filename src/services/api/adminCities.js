d424a56701c94be4624250ef094bc776ee58a4ee
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-17 12:31:03 -0500

    adminCities api abstraction

---
+++ src/services/api/adminCities.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export default {
+  getCities(params) {
+    return axios.get('/cities', {
+      params: params
+    })
+  },
+  editCity(id, payload) {
+    return axios.patch(`/cities/${id}`, payload)
+  },
+  saveCity(payload) {
+    return axios.post('/cities/', payload)
+  },
+  deleteCity(id) {
+    return axios.delete(`/cities/${id}`)
+  }
+}
