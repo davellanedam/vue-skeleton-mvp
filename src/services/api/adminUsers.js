11bccebfbf43892f41ccdbdd9925f556afe440d6
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-17 12:31:16 -0500

    adminUsers api abstraction

---
+++ src/services/api/adminUsers.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export default {
+  getUsers(params) {
+    return axios.get('/users', {
+      params: params
+    })
+  },
+  editUser(id, payload) {
+    return axios.patch(`/users/${id}`, payload)
+  },
+  saveUser(payload) {
+    return axios.post('/users/', payload)
+  },
+  deleteUser(id) {
+    return axios.delete(`/users/${id}`)
+  }
+}
