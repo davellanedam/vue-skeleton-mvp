5dec3c7513abacb03c20bb11fe8a200101508ec4
Daniel Avellaneda <davellanedam@gmail.com>
2019-03-26 14:45:53 -0500

    Compare versions function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -85,3 +85,26 @@ export const buildSuccess = (
   commit(types.ERROR, null)
   resolve(resolveParam)
 }
+
+export const compareVersion = (v1, v2) => {
+  if (typeof v1 !== 'string') {
+    return false
+  }
+  if (typeof v2 !== 'string') {
+    return false
+  }
+  v1 = v1.split('.')
+  v2 = v2.split('.')
+  const k = Math.min(v1.length, v2.length)
+  for (let i = 0; i < k; ++i) {
+    v1[i] = parseInt(v1[i], 10)
+    v2[i] = parseInt(v2[i], 10)
+    if (v1[i] > v2[i]) {
+      return 1
+    }
+    if (v1[i] < v2[i]) {
+      return -1
+    }
+  }
+  return v1.length === v2.length ? 0 : v1.length < v2.length ? -1 : 1
+}
