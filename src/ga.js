a2f9e8602fa2268833c09c55b17b5b1b38af5144
Daniel Avellaneda <davellanedam@gmail.com>
2019-04-09 17:57:04 -0500

    Fix: Google Analytics only in production mode

--- src/ga.js
+++ src/ga.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import router from '@/router'
 import VueAnalytics from 'vue-analytics'
 
 // Use Google Analytics only in production mode
 if (process.env.NODE_ENV === 'production') {
   Vue.use(VueAnalytics, {
-    id: 'UA-137710319-1'
+    id: 'UA-137710319-1',
+    router
   })
 }
