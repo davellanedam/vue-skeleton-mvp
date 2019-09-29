0399f9e515c2739255545ecdeb8674222cf121c6
Daniel Avellaneda <davellanedam@gmail.com>
2019-09-29 17:47:58 -0400

    date-fns fix new version

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,9 +2,11 @@ import i18n from '@/plugins/i18n'
 import * as types from '@/store/mutation-types'
 import { isPast, format, parseISO } from 'date-fns'
 import { store } from '@/store'
+import { es, zhCN } from 'date-fns/locale'
 
 const localesDateFns = {
-  es: require('date-fns/locale/es')
+  es,
+  cn: zhCN
 }
 
 export const getFormat = (date, formatStr) => {
